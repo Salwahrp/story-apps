93f2476f1c0984b78b6abc03dfc37f632cd1188b
ifs21006-itdel <ifs21006@students.del.ac.id>
2025-06-02 09:23:03 +0700

    add node modules

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/story-app/', // Set base to match GitHub Pages repo name
+  base: '/story-apps/', // Set base to match GitHub Pages repo name
 }); 
